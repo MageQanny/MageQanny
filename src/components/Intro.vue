0696b91d01d184d1949a67c9af4b3faaf24edfce
MageQanny <30309260+MageQanny@users.noreply.github.com>
2020-10-22 01:35:33 +0800

    Added a temporay error message while the site is still in development

---
+++ src/components/Intro.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="console">
+    <transition name="typing-text">
+      <h3>
+        <span id="prompt">
+          mageqanny@netlify-app:$
+        </span>
+        <span id="typed-text">
+          {{introText}}
+        </span>
+      </h3>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { 
+  ref, 
+  // onMounted 
+} from "vue";
+
+export default {
+    setup(){
+    
+      const introText = ref('./mageqanny');
+
+      // onMounted();
+
+      return {
+        introText, 
+      }
+    }
+};
+</script>
+
+
+<style scoped>
+
+#console{
+  font-family: 'Source Code Pro', monospace;
+  font-size: 25px;
+  font-weight: 600;
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+}
+h3{
+  margin: 0;
+}
+#prompt{
+  color: green;
+}
+#typed-text{
+  color: greenyellow;
+}
+</style>
